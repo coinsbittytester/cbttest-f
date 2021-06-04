e27ea7f5bc6ead466c1f8f706b81ef59ad89f474
Coins Bitty <84514483+coinsbittytester@users.noreply.github.com>
2021-06-04 07:25:21 -0700

    g

--- uikit/dist/index.esm.js
+++ uikit/dist/index.esm.js
@@ -530,8 +530,8 @@ var Icon$11 = function (props) {
 
 var Icon$10 = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 24 24" }, props),
-        React.createElement("image", { width: 24, height: 24, href: "/images/coins/ICEBRK.png" })));
-};   
+        React.createElement("image", { width: 24, height: 24, href: "/images/ICEBRK.png" })));
+};
 
 var Icon$$ = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 512 512" }, props),
@@ -4036,8 +4036,8 @@ var Menu = function (_a) {
         React.createElement(StyledNav, { showMenu: showMenu },
             React.createElement(Logo$1, { isPushed: isPushed, togglePush: function () { return setIsPushed(function (prevState) { return !prevState; }); }, isDark: isDark, href: (_b = homeLink === null || homeLink === void 0 ? void 0 : homeLink.href) !== null && _b !== void 0 ? _b : "/" }),
             React.createElement(Flex, null,
-                React.createElement(UserBlock$1, { account: account, login: login, logout: logout })/* ,
-                profile && React.createElement(Avatar, { profile: profile }) */)),
+                React.createElement(UserBlock$1, { account: account, login: login, logout: logout }),
+                profile && React.createElement(Avatar, { profile: profile }))),
         React.createElement(BodyWrapper, null,
             React.createElement(Panel, { isPushed: isPushed, isMobile: isMobile, showMenu: showMenu, isDark: isDark, toggleTheme: toggleTheme, langs: langs, setLang: setLang, currentLang: currentLang, cakePriceUsd: cakePriceUsd, pushNav: setIsPushed, links: links }),
             React.createElement(Inner, { isPushed: isPushed, showMenu: showMenu }, children),
